bb826e302ab9ffa3be93d7aada97f414143de135
kostas-theo <ktheo@oneconcern.com>
2022-03-08 15:13:16 +0100

    disable finalise and clear

--- larbs.sh
+++ larbs.sh
@@ -306,5 +306,5 @@ newperms "%wheel ALL=(ALL) ALL #LARBS
 # temp-disable # %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/paru,/usr/bin/pacman -Syyuw --noconfirm"
 
 # Last message! Install complete!
-finalize
-clear
+# finalize
+# clear
